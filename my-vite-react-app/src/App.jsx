427097b4a1d8d1d1bbc70caee7be76d3b54f5971
vinod <vinod20010330@gmail.com>
2024-08-09 01:50:06 +0530

    commit

--- my-vite-react-app/src/App.jsx
+++ my-vite-react-app/src/App.jsx
@@ -1,5 +1,5 @@
 import Assessment from "./Assessment"
-import { jsQuizz } from "./constants"
+import {jsQuizz} from "./constants.json"
 function App() {
     return <Assessment questions={jsQuizz.questions} />
 }
