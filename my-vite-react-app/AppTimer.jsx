cc3e5437380000bb70c65af2f904605e5de67692
vinod <vinod20010330@gmail.com>
2024-08-13 23:39:41 +0530

    timer

--- my-vite-react-app/AppTimer.jsx
+++ my-vite-react-app/AppTimer.jsx
@@ -1,33 +1,34 @@
 import "./AppTimer.scss";
-import { useEffect,useState,useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function AppTimer({ duration, onTimeUp }) {
-    const [counter, setCounter] = useState(0);
-    const [progressLoaded, setProgressLoaded] = useState(0);
-    const intervalRef = useRef();
-  
-    useEffect(() => {
-      intervalRef.current = setInterval(() => {
-        setCounter((cur) => cur + 1);
+  const [counter, setCounter] = useState(0);
+  const [progressLoaded, setProgressLoaded] = useState(0);
+  const intervalRef = useRef();
+
+  useEffect(() => {
+    intervalRef.current = setInterval(() => {
+      setCounter((cur) => cur + 1);
+    }, 1000);
+
+    return () => clearInterval(intervalRef.current);
+  }, []);
+
+  useEffect(() => {
+    setProgressLoaded(100 * (counter / duration));
+
+    if (counter === duration) {
+      clearInterval(intervalRef.current);
+
+      setTimeout(() => {
+        onTimeUp();
       }, 1000);
-  
-      return () => clearInterval(intervalRef.current);
-    }, []);
-  
-    useEffect(() => {
-      setProgressLoaded(100 * (counter / duration));
-  
-      if (counter === duration) {
-        clearInterval(intervalRef.current);
-  
-        setTimeout(() => {
-          onTimeUp();
-        }, 1000);
-      }
-    }, [counter]);
-  
-    return (
-      <div className="answer-timer-container">
+    }
+  }, [counter, duration, onTimeUp]);
+
+  return (
+    <div className="app-timer-container">
+      <div className="progress-box">
         <div
           style={{
             width: `${progressLoaded}%`,
@@ -42,7 +43,8 @@ function AppTimer({ duration, onTimeUp }) {
           className="progress"
         ></div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
+
 export default AppTimer;
