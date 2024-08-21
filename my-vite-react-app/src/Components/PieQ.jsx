16a368396f98b6904e38debab98187b4832903fb
vinod <vinod20010330@gmail.com>
2024-08-22 00:41:10 +0530

    new code

--- my-vite-react-app/src/Components/PieQ.jsx
+++ my-vite-react-app/src/Components/PieQ.jsx
@@ -1,38 +1,4 @@
 
-// import React from 'react';
-// import { PieChart, Pie, Cell, Tooltip, Legend } from 'recharts';
-
-// const COLORS = ['#0088FE', '#FF8042'];
-
-// const PieQ = ({ correct, wrong }) => {
-//   const data = [
-//     { name: 'Correct', value: correct },
-//     { name: 'Wrong', value: wrong },
-//   ];
-
-//   return (
-//     <PieChart width={400} height={400}>
-       
-//       <Pie
-//         data={data}
-//         cx={200}
-//         cy={200}
-//         labelLine={false}
-//         outerRadius={150}
-//         fill="#8884d8"
-//         dataKey="value"
-//       >
-//         {data.map((entry, index) => (
-//           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-//         ))}
-//       </Pie>
-//       <Tooltip />
-//       <Legend layout="horizontal" align="center" verticalAlign="top" />
-//     </PieChart>
-//   );
-// };
-
-// export default PieQ;
 import React from 'react';
 import { PieChart, Pie, Cell, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
